40fbe25f77df3a030cc69865dbc27ef504ef9ad9
suenweek <roman.novatorov@gmail.com>
2018-10-15 19:51:44 +0300

    - Add `Team` constructors

--- app/team.py
+++ app/team.py
@@ -1,11 +1,32 @@
+from . import config
+
+
 class Team:
 
-    def __init__(self, name, color, spymaster, players):
+    def __init__(self, name, color, spymaster_id, player_ids):
         self.name = name
         self.color = color
-        self.spymaster_id = spymaster
-        self.player_ids = players
+        self.spymaster_id = spymaster_id
+        self.player_ids = player_ids
 
     @property
     def member_ids(self):
         return self.player_ids + [self.spymaster_id]
+
+    @classmethod
+    def blue(cls, spymaster_id, player_ids):
+        return cls(
+            name='Team Blue',
+            color=config.COLOR_BLUE,
+            spymaster_id=spymaster_id,
+            player_ids=player_ids
+        )
+
+    @classmethod
+    def red(cls, spymaster_id, player_ids):
+        return cls(
+            name='Team Red',
+            color=config.COLOR_RED,
+            spymaster_id=spymaster_id,
+            player_ids=player_ids
+        )
