7843cfd5fcb5977d02bd996f79c8d8005eebbaba
suenweek <roman.novatorov@gmail.com>
2018-10-15 17:17:31 +0300

    - High level game algorithm

--- app/game.py
+++ app/game.py
@@ -1,51 +1,3 @@
-"""
-# Pseudo code:
-
-async def code_names():
-    teams = make_teams()
-    b_team, r_team = teams
-
-    b_team.spymaster = random.choice(b_team)
-    r_team.spymaster = random.choice(r_team)
-
-    map = create_random_map()
-
-    winner = None
-    cur_team = b_team
-
-    def another_team():
-        return r_team if cur_team is b_team else b_team
-
-    for team in teams:
-        await send(team.spymaster, map.as_img(reveal=True))
-
-    while not winner:
-        await broadcast(map.as_img(reveal=False))
-        await broadcast(f'{cur_team}\'s turn')
-
-        mistake = False
-
-        word, n_cells = await msg_from(cur_team.spymaster)
-        assert n_cells.is_integer() and n_cells > 0
-
-        while not mistake and n_cells:
-            word = await msg_from(cur_team)
-
-            cell = map[word]
-            cell.flip()
-
-            if cell.color is not cur_team.color:
-                mistake = True
-
-                if cell.color is Color.BLACK:
-                    winner = another_team()
-
-        winner = winner or map.winner()
-        cur_team = another_team()
-
-    await broadcast(f'winner is {winner}')
-"""
-
 import trio
 
 from . import config
@@ -54,7 +6,7 @@ from .team import Team
 from .utils import resource
 
 
-class Game:
+class BaseGame:
 
     def __init__(self, conv_id, map):
         self.conv_id = conv_id
@@ -63,10 +15,16 @@ class Game:
         self.blue_team = None
         self.red_team = None
 
-    async def start(self):
-        await self.registration()
+        self.winner = None
+        self.cur_team = None
 
-    async def registration(self):
+    async def _broadcast(self, msg):
+        raise NotImplementedError
+
+    async def _send(self, peer_id, msg):
+        raise NotImplementedError
+
+    async def _msg_from(self, peer_id):
         raise NotImplementedError
 
     @classmethod
@@ -74,3 +32,76 @@ class Game:
         words = resource.words(config.WORD_LIST_NAME)
         map = Map.random(words=words)
         return cls(conv_id=conv_id, map=map)
+
+
+class Game(BaseGame):
+
+    async def start(self):
+        await self.registration()
+
+        await self.reveal_map_to_spymasters()
+        while not self.winner:
+            self.cur_team = self.another_team
+            await self.play_round()
+
+        await self.announce_winner()
+
+    async def registration(self):
+        raise NotImplementedError
+
+    async def play_round(self):
+        await self.show_map_to_teams()
+        await self.announce_cur_team()
+
+        word, attempts = await self.get_word_and_number_from_spymaster()
+
+        while not self.winner and attempts:
+            word = await self.get_word_from_cur_team()
+
+            cell = self.map[word]
+            cell.flip()
+
+            if cell.color is self.cur_team.color:
+                if self.map.all_flipped(self.cur_team.color):
+                    self.winner = self.cur_team
+                attempts -= 1
+
+            elif cell.color is self.another_team.color:
+                if self.map.all_flipped(self.another_team.color):
+                    self.winner = self.another_team
+
+            elif cell.color is config.COLOR_WHITE:
+                attempts = 0
+
+            elif cell.color is config.COLOR_BLACK:
+                self.winner = self.another_team
+
+            else:
+                raise RuntimeError('Unreachable')
+
+    @property
+    def another_team(self):
+        if self.cur_team is self.blue_team:
+            return self.red_team
+        return self.blue_team
+
+    @property
+    def teams(self):
+        return self.blue_team, self.red_team
+
+    async def reveal_map_to_spymasters(self):
+        raise NotImplementedError
+    async def show_map_to_teams(self):
+        raise NotImplementedError
+
+    async def announce_winner(self):
+        raise NotImplementedError
+
+    async def announce_cur_team(self):
+        raise NotImplementedError
+
+    async def get_word_and_number_from_spymaster(self):
+        raise NotImplementedError
+
+    async def get_word_from_cur_team(self):
+        raise NotImplementedError
